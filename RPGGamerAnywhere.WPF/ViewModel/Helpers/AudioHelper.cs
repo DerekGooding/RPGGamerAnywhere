9cc4d3d54a202edd8c36f4a26e8cae247fc6e590
Derek Gooding <djgooding@live.com>
2024-08-12 09:59:34 -0400

    minor tidy

--- RPGGamerAnywhere.WPF/ViewModel/Helpers/AudioHelper.cs
+++ RPGGamerAnywhere.WPF/ViewModel/Helpers/AudioHelper.cs
@@ -9,52 +9,23 @@ namespace RPGGamerAnywhere.WPF.ViewModel.Helpers;
 
 public static class AudioHelper
 {
-    public static readonly string SamplePath = Path.Combine(Environment.CurrentDirectory, @"View\Sample\sample-3s.mp3");
-
-    public static ImageSource UpdateGraphic()
+    public static ImageSource UpdateGraphic(string filePath)
     {
-        //selectedFile = song.Url;
-        RenderWaveform();
+        RenderWaveform(filePath);
         return source;
     }
 
     private static ImageSource source;
-    private static readonly string selectedFile = SamplePath;
     private static string imageFile;
-    private static readonly WaveFormRenderer waveFormRenderer = new();
-    private static readonly WaveFormRendererSettings standardSettings = new StandardWaveFormRendererSettings();
+    private static readonly WaveFormRenderer _waveFormRenderer = new();
+    private static readonly WaveFormRendererSettings _standardSettings = new StandardWaveFormRendererSettings();
 
-    private static IPeakProvider GetPeakProvider()
-    {
-        //switch (comboBoxPeakCalculationStrategy.SelectedIndex)
-        //{
-        //    case 0:
-        //        return new MaxPeakProvider();
-        //    case 1:
-        //        return new RmsPeakProvider((int)upDownBlockSize.Value);
-        //    case 2:
-        //        return new SamplingPeakProvider((int)upDownBlockSize.Value);
-        //    case 3:
-        //        return new AveragePeakProvider(4);
-        //    default:
-        //        throw new InvalidOperationException("Unknown calculation strategy");
-        //}
-        return new MaxPeakProvider();
-    }
+    private static IPeakProvider GetPeakProvider() => new MaxPeakProvider();
 
-    private static WaveFormRendererSettings GetRendererSettings()
-    {
-        //var settings = (WaveFormRendererSettings)comboBoxRenderSettings.SelectedItem;
-        //settings.TopHeight = (int)upDownTopHeight.Value;
-        //settings.BottomHeight = (int)upDownBottomHeight.Value;
-        //settings.Width = (int)upDownWidth.Value;
-        //settings.DecibelScale = checkBoxDecibels.Checked;
-        return standardSettings;
-    }
+    private static WaveFormRendererSettings GetRendererSettings() => _standardSettings;
 
-    private static void RenderWaveform()
+    private static void RenderWaveform(string filePath)
     {
-        if (selectedFile == null) return;
         var settings = GetRendererSettings();
         if (imageFile != null)
         {
@@ -64,16 +35,16 @@ public static class AudioHelper
         //labelRendering.Visible = true;
         //Enabled = false;
         var peakProvider = GetPeakProvider();
-        Task.Factory.StartNew(() => RenderThreadFunc(peakProvider, settings));
+        Task.Factory.StartNew(() => RenderThreadFunc(peakProvider, settings, filePath));
     }
 
-    private static void RenderThreadFunc(IPeakProvider peakProvider, WaveFormRendererSettings settings)
+    private static void RenderThreadFunc(IPeakProvider peakProvider, WaveFormRendererSettings settings, string filePath)
     {
         Image? image = null;
         try
         {
-            using var waveStream = new NAudio.Wave.WaveFileReader(selectedFile);
-            image = waveFormRenderer.Render(waveStream, new MaxPeakProvider(), new StandardWaveFormRendererSettings());
+            using var waveStream = new NAudio.Wave.WaveFileReader(filePath);
+            image = _waveFormRenderer.Render(waveStream, new MaxPeakProvider(), new StandardWaveFormRendererSettings());
         }
         catch { }
         new Thread(() => FinishedRender(image)).Start();
